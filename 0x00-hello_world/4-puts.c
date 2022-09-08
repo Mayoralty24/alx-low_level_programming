5b4b326eb98f0b771bffc59902ee7e9777f36d3e
Mayoralty24 <gbengamayowadavid@gmail.com>
2022-09-08 11:13:21 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ * main - Serves as the entry point for the program
+ * Return 0
+ */ 
+int main void(void)
+{
+	("Programming is like building a multilingual puzzles");
+	return (0);
+}
