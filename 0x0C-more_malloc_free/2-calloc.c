d105c2f2812a10e98ee8d5cc93929fe92d0d1cd2
Mayoralty24 <gbengamayowadavid@gmail.com>
2022-10-06 08:18:23 -0700

    _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,5 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
-
 /**
  * _memset - copy char
  * @s: string
@@ -15,9 +16,9 @@ char *_memset(char *s, char b, unsigned int n)
 	{
 		s[i] = b;
 	}
+			
 	return (s);
 }
-
 /**
  * _calloc - allocates memory for an array using malloc
  * @nmemb: n elements
@@ -29,11 +30,16 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	void *p;
 
 	if (nmemb == 0 || size == 0)
+	{
 		return (NULL);
+	}
 	p = malloc(nmemb * size);
 
 	if (p == NULL)
+	{
 		return (NULL);
+	}
 	_memset(p, 0, (nmemb * size));
+
 	return (p);
 }
