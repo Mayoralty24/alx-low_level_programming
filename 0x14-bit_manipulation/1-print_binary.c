a53aaba0fd4f2e9a0cfed01ecfbf93191bdcf3a9
Mayoralty24 <gbengamayowadavid@gmail.com>
2022-10-27 08:29:54 -0700

    1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,21 +1,11 @@
 #include "main.h"
-
 /**
- * print_binary - Prints the binary representation of a number
- * @n: number in decimal to be printed in binary
+ * print_binary - prints the binary representation of a numnber
+ * @n: unsigned long, number to be represented in binary
  */
-
 void print_binary(unsigned long int n)
 {
-        if (n == 0)
-                _putchar('0');
-        else
-        {
-                if (n >> 1)
-                {
-                        print_binary(n >> 1);
-                }
-                _putchar((n & 1) + '0');
-        }
-
+	if (n > 1)
+		print_binary(n >> 1);
+	(n & 1) ? _putchar('1') : _putchar('0');
 }
